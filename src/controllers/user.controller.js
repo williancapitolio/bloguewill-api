2e9b2e4aabc5cf0dbdc3cfc516d507c7e0a04903
Willian <williansilvap25@gmail.com>
2022-09-14 22:20:19 -0300

    simple user validate

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,19 @@
 const create = (req, res) => {
-    const user = req.body;
-    res.json(user);
+    const { name, username, email, password, avatar, background } = req.body;
+    if (!name || !username || !email || !password || !avatar || !background) {
+        res.status(400).json({ message: "Submit all fields for registration" });
+    }
+    res.status(201).json({
+        message: "User created successfully",
+        user: {
+            name,
+            username,
+            email,
+            //password,
+            avatar,
+            background
+        }
+    });
 };
 
 module.exports = {
