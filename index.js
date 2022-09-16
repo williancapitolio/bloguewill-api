9f2c0fd9250e9595dc31edd4a9ce9521c4b67b37
Willian <williansilvap25@gmail.com>
2022-09-15 21:15:01 -0300

    dotenv setup

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 const userRoute = require("./src/routes/user.route");
 
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use("/user", userRoute);
