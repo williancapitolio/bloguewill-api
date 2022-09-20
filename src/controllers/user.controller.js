a354b256b83ef77c3f22d6a1a64c8a2c277526c5
Willian <williansilvap25@gmail.com>
2022-09-19 22:26:13 -0300

    create update route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,8 +44,25 @@ const findById = async (req, res) => {
     res.status(200).send(user);
 };
 
+const update = async (req, res) => {
+    const { name, username, email, password, avatar, background } = req.body;
+    if (!name && !username && !email && !password && !avatar && !background) {
+        res.status(400).json({ message: "Submit at least one field for update" });
+    }
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "Invalid ID" });
+    }
+    const user = await userService.findByIdService(id);
+    if (!user) {
+        return res.status(400).send({ message: "User not found" });
+    }
+    await userService.updateService();
+};
+
 module.exports = {
     create,
     findAll,
     findById,
+    update,
 };
