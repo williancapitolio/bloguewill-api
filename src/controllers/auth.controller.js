17e80b667db8b886a2da85b23bb6fac7c22bd3a4
Willian <williansilvap25@gmail.com>
2022-09-21 22:33:23 -0300

    create auth service

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,16 @@
+import bcrypt from "bcrypt";
+import { loginService } from "../services/auth.service.js";
 
 const login = async (req, res) => {
-    res.status(200).send("Login route ok");
+    try {
+        const { email, password } = req.body;
+        const user = loginService(email);
+        const passwordIsValid = bcrypt.compare(password, user.password);
+
+        res.status(200).send(email);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
 };
 
 export { login };
---
+++ src/services/auth.service.js
@@ -0,0 +1,7 @@
+import User from "../models/User.js";
+
+const loginService = () => {
+
+};
+
+export { loginService };
