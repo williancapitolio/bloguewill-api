dfcf819262b82d7155112aac0fe32b6fc7d94a29
Willian <williansilvap25@gmail.com>
2022-09-21 22:54:44 -0300

    compare password with bcrypt on auth controller ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,10 +4,15 @@ import { loginService } from "../services/auth.service.js";
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
-        const user = loginService(email);
-        const passwordIsValid = bcrypt.compare(password, user.password);
-
-        res.status(200).send(email);
+        const user = await loginService(email);
+        if (!user) {
+            return res.status(404).send({ message: "Invalid user or password" });
+        }
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if (!passwordIsValid) {
+            return res.status(404).send({ message: "Invalid user or password" });
+        }
+        res.status(200).send({ message: "Login ok" });
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
 
-const loginService = (email) => User.findOne({email: email});;
+const loginService = (email) => User.findOne({ email: email }).select("+password");
 
 export { loginService };
