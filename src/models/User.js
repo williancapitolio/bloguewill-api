ec8404c4c1c9278f6326e25eff253a429ac95ab8
Willian <williansilvap25@gmail.com>
2022-09-21 21:47:29 -0300

    correction in user model

--- src/models/User.js
+++ src/models/User.js
@@ -13,11 +13,13 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        select: false
     },
     avatar: {
         type: String,
