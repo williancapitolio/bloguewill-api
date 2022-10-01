dde1e2f274f5a3ac569e776cbbf0773780165893
Willian <williansilvap25@gmail.com>
2022-10-01 14:56:50 -0300

    controller test ok

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -1,6 +1,18 @@
+import { createService, findAllService } from "../services/news.service.js";
+
 const create = async (req, res) => {
     try {
-
+        const {title, text, user} = req.body;
+        if (!title || !text || !user) {
+            res.status(400).json({message: "Not found"});
+        }
+        await createService({
+            title,
+            text,
+            banner,
+            id: "testeid"
+        });
+        res.status(201).json({ message: "OK" });
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
@@ -8,7 +20,7 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
     try {
-
+        res.status(200).json({ message: "OK" });
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
