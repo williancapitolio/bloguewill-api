fe84c2e87a34f02f6c745ac8c21179468c48c927
Willian <williansilvap25@gmail.com>
2022-10-20 21:41:51 -0300

    create route for search news by title

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { create, findAll, topNews, findById } from "../controllers/newsController.js";
+import { create, findAll, topNews, searchByTitle, findById } from "../controllers/newsController.js";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
 
 const route = Router();
@@ -7,6 +7,7 @@ const route = Router();
 route.post("/", authMiddleware, create);
 route.get("/", findAll);
 route.get("/top", topNews);
+route.get("/search", searchByTitle);
 route.get("/:id", findById);
 
 export default route;
