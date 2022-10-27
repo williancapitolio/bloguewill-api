3897c727d1f71ad41d7a102484a69c6ab7d64e83
Willian <williansilvap25@gmail.com>
2022-10-26 22:10:12 -0300

    create route by user to find all news posts of one user

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { create, findAll, topNews, searchByTitle, findById } from "../controllers/newsController.js";
+import { create, findAll, topNews, searchByTitle, byUser, findById } from "../controllers/newsController.js";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
 
 const route = Router();
@@ -8,6 +8,7 @@ route.post("/", authMiddleware, create);
 route.get("/", findAll);
 route.get("/top", topNews);
 route.get("/search", searchByTitle);
+route.get("/byUser", authMiddleware, byUser);
 route.get("/:id", authMiddleware, findById);
 
 export default route;
