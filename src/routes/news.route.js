4a6a95eccb67e233bbf0546cbfccb400c8df1474
Willian <williansilvap25@gmail.com>
2022-11-10 22:40:36 -0300

    create comment route

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -9,7 +9,8 @@ import {
     findById,
     update,
     erase,
-    like
+    like,
+    comment
 } from "../controllers/newsController.js";
 
 const route = Router();
@@ -23,5 +24,6 @@ route.get("/:id", authMiddleware, findById);
 route.patch("/:id", authMiddleware, update);
 route.delete("/:id", authMiddleware, erase);
 route.patch("/like/:id", authMiddleware, like);
+route.patch("/comments/:id", authMiddleware, comment);
 
 export default route;
