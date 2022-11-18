72ffc60f3c9a3b814598b57ce18f32dd12cdc9d1
Willian <williansilvap25@gmail.com>
2022-11-17 22:49:03 -0300

    swagger ok

--- src/routes/swagger.route.js
+++ src/routes/swagger.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const route = Router();
 
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json";
+import swaggerDocument from "../swagger.json" assert { type: "json" };
 
 route.use("/", swaggerUi.serve);
 route.get("/", swaggerUi.setup(swaggerDocument));
