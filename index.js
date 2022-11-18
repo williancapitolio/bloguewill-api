4b87a2a089da73e5eaf6996877a4aef46f3118ac
Willian <williansilvap25@gmail.com>
2022-11-17 22:12:30 -0300

    add swagger on index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import connectDatabase from "./src/database/db.js";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import newsRoute from "./src/routes/news.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -17,5 +18,6 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute);
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
