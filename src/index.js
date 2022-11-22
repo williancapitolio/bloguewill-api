45a2bf788d9132a647515057fee97bbe74ebfc71
Willian <williansilvap25@gmail.com>
2022-11-21 22:02:41 -0300

    update swagger route to cjs to deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import connectDatabase from "./database/db.js";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import newsRoute from "./routes/news.route.js";
-import swaggerRoute from "./routes/swagger.route.js";
+import swaggerRoute from "./routes/swagger.route.cjs";
 
 const port = process.env.PORT || 3001;
 const app = express();
